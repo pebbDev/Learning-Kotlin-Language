31b11dfde3b0b181bb79a7ac0ce49c71fa6389d3
Febriyadi <flutterlearning6@gmail.com>
2024-04-15 06:44:45 +0800

    add commit while and do while topic

---
+++ Control Flow/app/src/main/kotlin/com/febriyadi/While dan Do While.kt
@@ -0,0 +1,23 @@
+package com.febriyadi
+
+fun main (){
+    //while
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+    //
+    var counter1 = 8
+    while (counter1 <= 7){
+        println("Hello, World!")
+        counter++
+    }
+
+    //Do while
+    var counter2 = 1
+    do {
+        println("Hello, World!")
+        counter++
+    } while (counter2 <= 7)
+}
